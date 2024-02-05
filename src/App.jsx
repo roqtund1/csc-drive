ab91eece32d0cdf82ec47454914fa9391417802a
kachi <astrokachi@gmail.com>
2024-02-05 05:01:03 +0100

    feat: welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,4 @@
-// import { useEffect, useState } from "react";
-// import { Card } from "./components/Card";
-// import { Header } from "./components/Header";
-// import Sidebar from "./components/Sidebar";
-// import { collection, getDocs } from "firebase/firestore";
-// import { db } from "../firebase";
 
-// function App() {
-//   const [data, setData] = useState([]);
-//   const [filter, setFilter] = useState('');
-//   const [search, setSearch] = useState('');
-
-//   const getData = async () => {
-//     const valRef = collection(db, 'store');
-//     const dataDb = await getDocs(valRef);
-//     const allData = dataDb.docs.map(val => ({ ...val.data(), id: val.id }));
-//     const filteredData = allData.filter(val => val.tags.toString().toLowerCase().includes(filter.toLowerCase()));
-
-//     // Apply search filter
-//     const searchedData = filteredData.filter(val =>
-//       val.name.toLowerCase().includes(search.toLowerCase()) ||
-//       val.desc.toLowerCase().includes(search.toLowerCase()) ||
-//       val.tags.toString().toLowerCase().includes(search.toLowerCase())
-//     );
-
-//     filter ? setData(searchedData) : setData(allData);
-//     search && setData(searchedData)
-//   };
-
-//   useEffect(() => {
-//     getData();
-//     console.log(search);
-//   }, [filter, search]);
-
-//   return (
-//     <>
-//       <div className='bg-white text-black min-h-screen m-0'>
-//         <Header setSearch={setSearch} />
-//         {/* body  */}
-//         <div className="flex px-14 mt-10 pb-4 gap-5 flex-wrap">
-//           <div className="md:w-[60%] w-full flex flex-col gap-3">
-//             {data.map((val, index) => (
-//               <Card key={index} name={val.name} tags={val.tags} desc={val.desc} fileUrl={val.fileUrl} />
-//             ))}
-//           </div>
-//           <div className="md:w-[35%] w-full">
-//             <Sidebar setFilter={setFilter} />
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default App;
 
 import { useEffect, useState } from "react";
 import { Card } from "./components/Card";
@@ -148,7 +94,7 @@ function App() {
               ))}
             </div>
           ) : (
-            <div className="w-full mt-44 absolute bg-slate-300 h-screen z-10 bottom-0 left-0 pt-52 books" >
+            <div className="w-full mt-32 absolute bg-slate-300 h-screen z-10 bottom-0 left-0 pt-52 books" >
             <div className="text-center border w-max py-20 px-8 rounded-sm mx-auto bg-white">
               <div className="mx-auto w-max">
               <svg width="208" height="31" viewBox="0 0 208 31" fill="none" xmlns="http://www.w3.org/2000/svg">
